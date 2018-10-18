2014c6af6d4d229db52440d2af9d6427d8d0ef6d
Your Name <you@example.com>
2018-10-18 20:57:37 +0400

    gitignore file

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,11 +1,28 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
+
 public class LoginTest {
+    private WebDriver driver;
+
     @Test
     public void successLogin() {
         System.setProperty("webdriver.chrome.driver" , "C:\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.get("https://google.com");
+        assertTrue(driver.getCurrentUrl().contains("https://www.google.com"),
+                "The page was not load correctly");
+
+
+
     }
+    @AfterMethod
+    public void tearDown() {
+    driver.quit();
+}
+
 }
